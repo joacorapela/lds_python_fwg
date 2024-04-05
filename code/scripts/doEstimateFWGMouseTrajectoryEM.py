2650e64439da80f9975445df203471093da140f8
Joaquin Rapela <joacorapela@gmail.com>
2024-04-05 10:34:24 +0100

    Tidied code; several updates

--- code/scripts/doEstimateFWGMouseTrajectoryEM.py
+++ code/scripts/doEstimateFWGMouseTrajectoryEM.py
@@ -7,17 +7,16 @@ import random
 import pickle
 import numpy as np
 import scipy.interpolate
-import scipy.stats
 import pandas as pd
 
-import lds_python.learning
+import lds.learning
 
 
 def main(argv):
     parser = argparse.ArgumentParser()
-    parser.add_argument("estMeta_number", help="estimation metadata number",
-                        type=int)
-    parser.add_argument("--skip_estimation_sqrt_noise_intensity",
+    parser.add_argument("--estMeta_number", help="estimation metadata number",
+                        type=int, default=25)
+    parser.add_argument("--skip_estimation_sigma_a",
                         help=("use this option to skip the estimation of the "
                               "sqrt noise inensity"), action="store_true")
     parser.add_argument("--skip_estimation_R",
@@ -33,7 +32,7 @@ def main(argv):
                         default="../../metadata/{:08d}_estimation.ini",
                         help="estimation initialization metadata filename pattern")
     parser.add_argument("--data_filename", type=str,
-                        default="../../data/positions_session003_start0.00_end15548.27.csv",
+                        default="~/gatsby-swc/fwg/repos/aeon_repo/results/interpolated_NaNs_positions_2022-08-15T13:11:23.791259766_0_10743.csv",
                         help="inputs positions filename")
     parser.add_argument("--estRes_metadata_filename_pattern", type=str,
                         default="../../results/{:08d}_estimation.ini",
@@ -44,8 +43,7 @@ def main(argv):
     args = parser.parse_args()
 
     estMeta_number = args.estMeta_number
-    skip_estimation_sqrt_noise_intensity = \
-        args.skip_estimation_sqrt_noise_intensity
+    skip_estimation_sigma_a = args.skip_estimation_sigma_a
     skip_estimation_R = args.skip_estimation_R
     skip_estimation_m0 = args.skip_estimation_m0
     skip_estimation_V0 = args.skip_estimation_V0
@@ -59,73 +57,102 @@ def main(argv):
 
     estMeta = configparser.ConfigParser()
     estMeta.read(estInit_metadata_filename)
-    start_position = int(estMeta["data_params"]["start_position"])
-    number_positions = int(estMeta["data_params"]["number_positions"])
+    start_offset_secs = int(estMeta["data_params"]["start_offset_secs"])
+    duration_secs = int(estMeta["data_params"]["duration_secs"])
     pos_x0 = float(estMeta["initial_params"]["pos_x0"])
     pos_y0 = float(estMeta["initial_params"]["pos_y0"])
     vel_x0 = float(estMeta["initial_params"]["vel_x0"])
     vel_y0 = float(estMeta["initial_params"]["vel_y0"])
     ace_x0 = float(estMeta["initial_params"]["ace_x0"])
     ace_y0 = float(estMeta["initial_params"]["ace_y0"])
-    sqrt_noise_intensity0 = float(estMeta["initial_params"]["sqrt_noise_intensity"])
+    sigma_a0 = float(estMeta["initial_params"]["sigma_a"])
     sigma_x0 = float(estMeta["initial_params"]["sigma_x"])
     sigma_y0 = float(estMeta["initial_params"]["sigma_y"])
     sqrt_diag_V0_value = float(estMeta["initial_params"]["sqrt_diag_v0_value"])
     em_max_iter = int(estMeta["optim_params"]["em_max_iter"])
     Qe_reg_param = float(estMeta["optim_params"]["em_Qe_reg_param"])
 
-    data = pd.read_csv(filepath_or_buffer=data_filename)
-    data = data.iloc[start_position:start_position+number_positions,:]
-    y = np.transpose(data[["x", "y"]].to_numpy())
-    date_times = pd.to_datetime(data["time"])
-    dt = (date_times.iloc[1]-date_times.iloc[0]).total_seconds()
+    data = pd.read_csv(data_filename, header=None)
+    ts = data[1]
+    dt = (pd.to_datetime(ts.iloc[1])-pd.to_datetime(ts.iloc[0])).total_seconds()
+    start_position = int(start_offset_secs / dt)
+    if duration_secs < 0:
+        number_positions = data.shape[0] - start_position
+    else:
+        number_positions = int(duration_secs / dt)
+    data = data.iloc[
+        start_position:(start_position+number_positions), :]
+    timestamps = pd.to_datetime(data[1].to_numpy())
+    x = data[2].to_numpy()
+    y = data[3].to_numpy()
+    pos = np.vstack((x, y))
 
-    times = np.arange(0, y.shape[1]*dt, dt)
-    not_nan_indices_y0 = set(np.where(np.logical_not(np.isnan(y[0, :])))[0])
-    not_nan_indices_y1 = set(np.where(np.logical_not(np.isnan(y[1, :])))[0])
-    not_nan_indices = np.array(sorted(not_nan_indices_y0.union(not_nan_indices_y1)))
-    y_no_nan = y[:, not_nan_indices]
+    # make sure that the first data point is not NaN
+    first_not_nan_index = np.where(np.logical_and(
+        np.logical_not(np.isnan(x)), np.logical_not(np.isnan(y))))[0][0]
+    timestamps = timestamps[first_not_nan_index:]
+    pos = pos[:, first_not_nan_index:]
+    #
+
+    # tmp code
+    import plotly.graph_objects as go
+
+    fig = go.Figure()
+    trace = go.Scatter(x=pos[0, :], y=pos[1, :], mode="markers")
+    fig.add_trace(trace)
+    fig.update_layout(title="Before interpolation",
+                      xaxis=dict(title="x (pixels)"),
+                      yaxis=dict(title="y (pixels"))
+    fig.show()
+    #
+
+    breakpoint()
+    # interpolate nan
+    times = (timestamps - timestamps[0]).total_seconds().to_numpy()
+    not_nan_indices_x = set(np.where(np.logical_not(np.isnan(pos[0, :])))[0])
+    not_nan_indices_y = set(np.where(np.logical_not(np.isnan(pos[1, :])))[0])
+    not_nan_indices = np.array(sorted(not_nan_indices_x.union(not_nan_indices_y)))
+    pos_no_nan = pos[:, not_nan_indices]
     t_no_nan = times[not_nan_indices]
-    y_interpolated = np.empty_like(y)
-    tck, u = scipy.interpolate.splprep([y_no_nan[0, :], y_no_nan[1, :]], s=0, u=t_no_nan)
-    y_interpolated[0, :], y_interpolated[1, :] = scipy.interpolate.splev(times, tck)
-    y = y_interpolated
-    # y = y[:, :1000]
+    pos_interpolated = np.empty_like(pos)
+    tck, u = scipy.interpolate.splprep([pos_no_nan[0, :], pos_no_nan[1, :]], s=0, u=t_no_nan)
+    pos_interpolated[0, :], pos_interpolated[1, :] = scipy.interpolate.splev(times, tck)
+    pos = pos_interpolated
+    #
+
+    # tmp code
+    import plotly.graph_objects as go
+
+    fig = go.Figure()
+    trace = go.Scatter(x=pos[0, :], y=pos[1, :], mode="markers")
+    fig.add_trace(trace)
+    fig.update_layout(title="After interpolation",
+                      xaxis=dict(title="x (pixels)"),
+                      yaxis=dict(title="y (pixels"))
+    fig.show()
+    #
 
     if math.isnan(pos_x0):
-        pos_x0 = y[0, 0]
+        pos_x0 = pos[0, 0]
     if math.isnan(pos_y0):
-        pos_y0 = y[1, 0]
+        pos_y0 = pos[1, 0]
 
-    # Taken from the book
-    # barShalomEtAl01-estimationWithApplicationToTrackingAndNavigation.pdf
-    # section 6.2.3
-    # Eq. 6.2.3-7
-    B = np.array([[1, dt, .5*dt**2, 0, 0, 0],
-                  [0, 1,  dt,       0, 0, 0],
-                  [0, 0,  1,        0, 0, 0],
-                  [0, 0,  0,        1, dt, .5*dt**2],
-                  [0, 0,  0,        0, 1,  dt],
-                  [0, 0,  0,        0, 0,  1]], dtype=np.double)
-    Z = np.array([[1, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 1, 0, 0]], dtype=np.double)
-    # Eq. 6.2.3-8
-    Qe = np.array([[dt**5/20, dt**4/8, dt**3/6, 0, 0, 0],
-                   [dt**4/8, dt**3/3,  dt**2/2, 0, 0, 0],
-                   [dt**3/6, dt**2/2,  dt,      0, 0, 0],
-                   [0, 0, 0,                    dt**5/20, dt**4/8, dt**3/6],
-                   [0, 0, 0,                    dt**4/8, dt**3/3,  dt**2/2],
-                   [0, 0, 0,                    dt**3/6, dt**2/2,  dt]],
-                  dtype=np.double)
+    B, Q, Z, R_0, Qe = lds.tracking.utils.getLDSmatricesForTracking(dt=dt,
+                                                                    sigma_a=sigma_a0,
+                                                                    sigma_x=sigma_x0,
+                                                                    sigma_y=sigma_y0)
     Qe += Qe_reg_param * np.eye(Qe.shape[0])
     m0 = np.array([pos_x0, vel_x0, ace_x0, pos_y0, vel_y0, ace_y0],
                   dtype=np.double)
+    m0_0 = m0
+    sqrt_diag_V0 = np.array([sqrt_diag_V0_value for i in range(len(m0))])
+    V0_0 = np.diag(sqrt_diag_V0)
 
     vars_to_estimate = {}
-    if skip_estimation_sqrt_noise_intensity:
-        vars_to_estimate["sqrt_noise_intensity"] = False
+    if skip_estimation_sigma_a:
+        vars_to_estimate["sigma_a"] = False
     else:
-        vars_to_estimate["sqrt_noise_intensity"] = True
+        vars_to_estimate["sigma_a"] = True
 
     if skip_estimation_R:
         vars_to_estimate["R"] = False
@@ -142,14 +169,8 @@ def main(argv):
     else:
         vars_to_estimate["V0"] = True
 
-    sqrt_diag_R = np.array([sigma_x0, sigma_y0])
-    R_0 = np.diag(sqrt_diag_R)
-    m0_0 = m0
-    sqrt_diag_V0 = np.array([sqrt_diag_V0_value for i in range(len(m0))])
-    V0_0 = np.diag(sqrt_diag_V0)
-
-    optim_res  = lds_python.learning.em_SS_tracking(
-        y=y, B=B, sqrt_noise_intensity0=sqrt_noise_intensity0,
+    optim_res  = lds.learning.em_SS_tracking(
+        y=pos, B=B, sigma_a0=sigma_a0,
         Qe=Qe, Z=Z, R_0=R_0, m0_0=m0_0, V0_0=V0_0,
         vars_to_estimate=vars_to_estimate,
         max_iter=em_max_iter)
