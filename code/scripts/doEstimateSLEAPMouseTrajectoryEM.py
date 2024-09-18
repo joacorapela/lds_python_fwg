852feb2cb0f430c8042a1b48bacdb4875b4d858d
Joaquin Rapela <joacorapela@gmail.com>
2024-09-18 15:43:13 +0100

    Added

---
+++ code/scripts/doEstimateSLEAPMouseTrajectoryEM.py
@@ -0,0 +1,205 @@
+import sys
+import os.path
+import argparse
+import configparser
+import math
+import random
+import pickle
+import numpy as np
+import scipy.interpolate
+import pandas as pd
+
+import lds.learning
+
+
+def main(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--estMeta_number", help="estimation metadata number",
+                        type=int, default=33)
+    parser.add_argument("--skip_estimation_sigma_a",
+                        help=("use this option to skip the estimation of the "
+                              "sqrt noise inensity"), action="store_true")
+    parser.add_argument("--skip_estimation_R",
+                        help="use this option to skip the estimation of R",
+                        action="store_true")
+    parser.add_argument("--skip_estimation_m0",
+                        help="use this option to skip the estimation of m0",
+                        action="store_true")
+    parser.add_argument("--skip_estimation_V0",
+                        help="use this option to skip the estimation of V0",
+                        action="store_true")
+    parser.add_argument("--estInit_metadata_filename_pattern", type=str,
+                        default="../../metadata/{:08d}_estimation.ini",
+                        help="estimation initialization metadata filename pattern")
+    parser.add_argument("--data_filename", type=str,
+                        default="/nfs/gatsbystor/rapela/fwg/positions_noOutliers_AEON3_2024-02-05T15-00-00_2024-02-08T14-00-00.csv",
+                        # default="~/gatsby-swc/fwg/repos/aeon_repo/results/positions_BAA-1104047_2024-02-05 15:00:00.000000_2024-02-05 16:00:00.000000_2024-02-05 17:00:00.000000.csv",
+                        help="inputs positions filename")
+    parser.add_argument("--estRes_metadata_filename_pattern", type=str,
+                        default="../../results/{:08d}_estimation.ini",
+                        help="estimation results metadata filename pattern")
+    parser.add_argument("--estRes_data_filename_pattern", type=str,
+                        default="../../results/{:08d}_estimation.pickle",
+                        help="estimation results data filename pattern")
+    args = parser.parse_args()
+
+    estMeta_number = args.estMeta_number
+    skip_estimation_sigma_a = args.skip_estimation_sigma_a
+    skip_estimation_R = args.skip_estimation_R
+    skip_estimation_m0 = args.skip_estimation_m0
+    skip_estimation_V0 = args.skip_estimation_V0
+    estInit_metadata_filename_pattern = args.estInit_metadata_filename_pattern
+    data_filename = args.data_filename
+    estRes_metadata_filename_pattern = args.estRes_metadata_filename_pattern
+    estRes_data_filename_pattern = args.estRes_data_filename_pattern
+
+    estInit_metadata_filename = \
+        estInit_metadata_filename_pattern.format(estMeta_number)
+
+    estMeta = configparser.ConfigParser()
+    estMeta.read(estInit_metadata_filename)
+    start_time_str = (estMeta["data_params"]["start_time"])
+    end_time_str = (estMeta["data_params"]["end_time"])
+    pos_x0 = float(estMeta["initial_params"]["pos_x0"])
+    pos_y0 = float(estMeta["initial_params"]["pos_y0"])
+    vel_x0 = float(estMeta["initial_params"]["vel_x0"])
+    vel_y0 = float(estMeta["initial_params"]["vel_y0"])
+    ace_x0 = float(estMeta["initial_params"]["ace_x0"])
+    ace_y0 = float(estMeta["initial_params"]["ace_y0"])
+    sigma_a0 = float(estMeta["initial_params"]["sigma_a"])
+    sigma_x0 = float(estMeta["initial_params"]["sigma_x"])
+    sigma_y0 = float(estMeta["initial_params"]["sigma_y"])
+    sqrt_diag_V0 = np.array(
+        [float(sqrt_diag_v0_value_str)
+         for sqrt_diag_v0_value_str in estMeta["initial_params"]["sqrt_diag_V0"][1:-1].split(",")]
+    )
+    em_max_iter = int(estMeta["optim_params"]["em_max_iter"])
+    Qe_reg_param = float(estMeta["optim_params"]["em_Qe_reg_param"])
+
+    # data = pd.read_csv(data_filename, header=False)
+    data = pd.read_csv(data_filename, index_col=0)
+    data.index = pd.to_datetime(data.index)
+    ts = data.index
+    dt = (pd.to_datetime(ts[1])-pd.to_datetime(ts[0])).total_seconds()
+    start_time = pd.to_datetime(start_time_str, utc=True)
+    end_time = pd.to_datetime(end_time_str, utc=True)
+    data = data.iloc[np.logical_and(data.index>=start_time,
+                                    data.index<=end_time)]
+    timestamps = pd.to_datetime(data.index.to_numpy())
+    x = data["spine2_x"].to_numpy()
+    y = data["spine2_y"].to_numpy()
+    pos = np.vstack((x, y))
+
+    # make sure that the first data point is not NaN
+    first_not_nan_index = np.where(np.logical_and(
+        np.logical_not(np.isnan(x)), np.logical_not(np.isnan(y))))[0][0]
+    timestamps = timestamps[first_not_nan_index:]
+    pos = pos[:, first_not_nan_index:]
+    #
+
+    # tmp code
+    # import plotly.graph_objects as go
+
+    # fig = go.Figure()
+    # trace = go.Scatter(x=pos[0, :], y=pos[1, :], mode="markers")
+    # fig.add_trace(trace)
+    # fig.update_layout(title="Before interpolation",
+    #                   xaxis=dict(title="x (pixels)"),
+    #                   yaxis=dict(title="y (pixels"))
+    # fig.show()
+    #
+
+    # breakpoint()
+    # interpolate nan
+    times = (timestamps - timestamps[0]).total_seconds().to_numpy()
+    not_nan_indices_x = set(np.where(np.logical_not(np.isnan(pos[0, :])))[0])
+    not_nan_indices_y = set(np.where(np.logical_not(np.isnan(pos[1, :])))[0])
+    not_nan_indices = np.array(sorted(not_nan_indices_x.union(not_nan_indices_y)))
+    pos_no_nan = pos[:, not_nan_indices]
+    t_no_nan = times[not_nan_indices]
+    pos_interpolated = np.empty_like(pos)
+    tck, u = scipy.interpolate.splprep([pos_no_nan[0, :], pos_no_nan[1, :]], s=0, u=t_no_nan)
+    pos_interpolated[0, :], pos_interpolated[1, :] = scipy.interpolate.splev(times, tck)
+    pos = pos_interpolated
+    #
+
+    # tmp code
+    # import plotly.graph_objects as go
+
+    # fig = go.Figure()
+    # trace = go.Scatter(x=pos[0, :], y=pos[1, :], mode="markers")
+    # fig.add_trace(trace)
+    # fig.update_layout(title="After interpolation",
+    #                   xaxis=dict(title="x (pixels)"),
+    #                   yaxis=dict(title="y (pixels"))
+    # fig.show()
+    #
+
+    if math.isnan(pos_x0):
+        pos_x0 = pos[0, 0]
+    if math.isnan(pos_y0):
+        pos_y0 = pos[1, 0]
+
+    B, Q, Z, R_0, Qe = lds.tracking.utils.getLDSmatricesForTracking(dt=dt,
+                                                                    sigma_a=sigma_a0,
+                                                                    sigma_x=sigma_x0,
+                                                                    sigma_y=sigma_y0)
+    Qe += Qe_reg_param * np.eye(Qe.shape[0])
+    m0 = np.array([pos_x0, vel_x0, ace_x0, pos_y0, vel_y0, ace_y0],
+                  dtype=np.double)
+    m0_0 = m0
+    V0_0 = np.diag(sqrt_diag_V0)
+
+    vars_to_estimate = {}
+    if skip_estimation_sigma_a:
+        vars_to_estimate["sigma_a"] = False
+    else:
+        vars_to_estimate["sigma_a"] = True
+
+    if skip_estimation_R:
+        vars_to_estimate["R"] = False
+    else:
+        vars_to_estimate["R"] = True
+
+    if skip_estimation_m0:
+        vars_to_estimate["m0"] = False
+    else:
+        vars_to_estimate["m0"] = True
+
+    if skip_estimation_V0:
+        vars_to_estimate["V0"] = False
+    else:
+        vars_to_estimate["V0"] = True
+
+    optim_res  = lds.learning.em_SS_tracking(
+        y=pos, B=B, sigma_a0=sigma_a0,
+        Qe=Qe, Z=Z, R_0=R_0, m0_0=m0_0, V0_0=V0_0,
+        vars_to_estimate=vars_to_estimate,
+        max_iter=em_max_iter)
+
+    print(optim_res["termination_info"])
+
+    # save results
+    est_prefix_used = True
+    while est_prefix_used:
+        estRes_number = random.randint(0, 10**8)
+        estRes_metadata_filename = \
+            estRes_metadata_filename_pattern.format(estRes_number)
+        if not os.path.exists(estRes_metadata_filename):
+            est_prefix_used = False
+    estRes_data_filename = estRes_data_filename_pattern.format(estRes_number)
+
+    estimRes_metadata = configparser.ConfigParser()
+    estimRes_metadata["data_params"] = {"data_filename": data_filename}
+    estimRes_metadata["estimation_params"] = {"estInitNumber": estMeta_number}
+    with open(estRes_metadata_filename, "w") as f:
+        estimRes_metadata.write(f)
+
+    with open(estRes_data_filename, "wb") as f:
+        pickle.dump(optim_res, f)
+    print("Saved results to {:s}".format(estRes_data_filename))
+
+    breakpoint()
+
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ code/scripts/doSmoothSLEAPMouseTrajectory.py
@@ -0,0 +1,134 @@
+import sys
+import os
+import random
+import pickle
+import math
+import argparse
+import configparser
+import numpy as np
+import pandas as pd
+
+import lds.tracking.utils
+import lds.inference
+
+
+def main(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--start_offset_secs", type=int, default=0,
+                        help="start offset in seconds")
+    parser.add_argument("--duration_secs", type=int, default=-1,
+                        help="duration in seconds")
+    parser.add_argument("--filtering_params_filename", type=str,
+                        default="../../metadata/00000012_smoothing.ini",
+                        help="filtering parameters filename")
+    parser.add_argument("--filtering_params_section", type=str,
+                        default="params",
+                        help=("section of ini file containing the filtering "
+                              "params"))
+    parser.add_argument(
+        "--data_filename", type=str,
+        default="~/gatsby-swc/fwg/repos/aeon_repo/results/positions_AEON3_2024-02-05T15-00-00_2024-02-05T16-00-00_2024-02-05T17-00-00.csv",
+        help="inputs positions filename")
+    parser.add_argument("--results_filename_pattern", type=str,
+                        default="../../results/{:08d}_smoothed.{:s}")
+    args = parser.parse_args()
+
+    start_offset_secs = args.start_offset_secs
+    duration_secs = args.duration_secs
+    filtering_params_filename = args.filtering_params_filename
+    filtering_params_section = args.filtering_params_section
+    data_filename = args.data_filename
+    results_filename_pattern = args.results_filename_pattern
+
+    data = pd.read_csv(filepath_or_buffer=data_filename, header=0)
+    ts = data.iloc[:,0]
+    dt = (pd.to_datetime(ts.iloc[1])-pd.to_datetime(ts.iloc[0])).total_seconds()
+    start_position = int(start_offset_secs / dt)
+    if duration_secs < 0:
+        number_positions = data.shape[0] - start_position
+    else:
+        number_positions = int(duration_secs / dt)
+    data = data.iloc[
+        start_position:(start_position+number_positions), :]
+    timestamps = data.iloc[:, 0].to_numpy()
+    x = data["spine2_x"].to_numpy()
+    y = data["spine2_y"].to_numpy()
+    pos = np.vstack((x, y))
+
+    # make sure that the first data point is not NaN
+    first_nan_index = np.where(np.logical_and(
+        np.logical_not(np.isnan(x)), np.logical_not(np.isnan(y))))[0][0]
+    timestamps = timestamps[first_nan_index:]
+    pos = pos[:, first_nan_index:]
+    #
+
+    smoothing_params = configparser.ConfigParser()
+    smoothing_params.read(filtering_params_filename)
+    pos_x0 = float(smoothing_params[filtering_params_section]["pos_x0"])
+    pos_y0 = float(smoothing_params[filtering_params_section]["pos_y0"])
+    vel_x0 = float(smoothing_params[filtering_params_section]["vel_x0"])
+    vel_y0 = float(smoothing_params[filtering_params_section]["vel_x0"])
+    acc_x0 = float(smoothing_params[filtering_params_section]["acc_x0"])
+    acc_y0 = float(smoothing_params[filtering_params_section]["acc_x0"])
+    sigma_a = float(smoothing_params[filtering_params_section]["sigma_a"])
+    sigma_x = float(smoothing_params[filtering_params_section]["sigma_x"])
+    sigma_y = float(smoothing_params[filtering_params_section]["sigma_y"])
+    diag_V0_values = np.array(
+        [float(diag_value) for diag_value in
+         smoothing_params[filtering_params_section]
+         ["diag_V0_values"][1:-1].split(",")])
+
+    if math.isnan(pos_x0):
+        pos_x0 = pos[0, 0]
+    if math.isnan(pos_y0):
+        pos_y0 = pos[1, 0]
+
+    m0 = np.array([pos_x0, vel_x0, acc_x0, pos_y0, vel_y0, acc_y0],
+                  dtype=np.double)
+    V0 = np.diag(diag_V0_values)
+    R = np.diag([sigma_x**2, sigma_y**2])
+
+    B, Q, Z, R, Qt = lds.tracking.utils.getLDSmatricesForTracking(dt=dt,
+                                                                  sigma_a=sigma_a,
+                                                                  sigma_x=sigma_x,
+                                                                  sigma_y=sigma_y)
+    filter_res = lds.inference.filterLDS_SS_withMissingValues_np(
+        y=pos, B=B, Q=Q, m0=m0, V0=V0, Z=Z, R=R)
+    smooth_res = lds.inference.smoothLDS_SS(
+        B=B, xnn=filter_res["xnn"], Vnn=filter_res["Vnn"],
+        xnn1=filter_res["xnn1"], Vnn1=filter_res["Vnn1"],
+        m0=m0, V0=V0)
+    results = {"timestamps": timestamps,
+               "measurements": pos,
+               "filter_res": filter_res,
+               "smooth_res": smooth_res}
+
+    # save results
+    res_prefix_used = True
+    while res_prefix_used:
+        res_number = random.randint(0, 10**8)
+        metadata_filename = results_filename_pattern.format(res_number, "ini")
+        if not os.path.exists(metadata_filename):
+            res_prefix_used = False
+    results_filename = results_filename_pattern.format(res_number, "pickle")
+
+    with open(results_filename, "wb") as f:
+        pickle.dump(results, f)
+    print(f"Saved smoothing results to {results_filename}")
+
+    metadata = configparser.ConfigParser()
+    metadata["params"] = {
+        "data_filename": data_filename,
+        "start_position": start_position,
+        "number_positions": number_positions,
+        "filtering_params_filename": filtering_params_filename,
+        "filtering_params_section": filtering_params_section,
+    }
+    with open(metadata_filename, "w") as f:
+        metadata.write(f)
+
+    breakpoint()
+
+
+if __name__ == "__main__":
+    main(sys.argv)
